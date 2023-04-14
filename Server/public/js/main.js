f2c080a13ddd68c9020b5c67c1736c3e6dc66b9d
Dana Alhaji <dana.alhaji95@gmail.com>
2023-04-15 00:17:36 +0300

    add emit and on for both sides

--- Server/public/js/main.js
+++ Server/public/js/main.js
@@ -1,1 +1,28 @@
 const socket = io(); // we have access on it because of the script tag we are added in the chat.hmtl file 
+
+const chatFrom = document.getElementById('chat-form');
+
+// message from server
+socket.on('message', message =>{
+    console.log(message);
+    outputMessage(message);
+})
+
+// message submit
+
+chatFrom.addEventListener('submit', e =>{
+
+    e.preventDefault();
+    const msg = e.target.elements.msg.value;
+
+    // Emit message to server
+    socket.emit('messageChat', msg);
+})
+
+// Output message to DOM
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = '
+    '
+}
--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket =>{
     console.log("New Websocket Connection"); // when ever a client connects it will console log this 
-    // emit events back and furth 
+    // emit events back and forth 
+    // welcome current user
+    socket.emit('message','Welcome to chaitChat')
+    
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Runs when the client left the chat
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+    // Listen to chatMessage
+    socket.on('messageChat', msg => {
+        io.emit('message', msg);
+    });
+
 });// listen for some kind of event 
 
 const PORT = 3000 || process.env.PORT;
